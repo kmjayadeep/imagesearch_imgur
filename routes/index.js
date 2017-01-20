e303c2d79e7d0f164bdd7874a4271a4aba652b97
Jayadeep KM <kmjayadeep@gmail.com>
2017-01-20 21:29:47 +0530

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.json')
+var request = require('request')
 
-router.get('/',function(req,res){
-	res.render('index',{title:'express'})
+router.get('/', function(req, res) {
+    res.render('index', { title: 'express' })
 })
 
 var history = []
 
 router.get('/api/imagesearch/:query', function(req, res, next) {
-	history.push({
-		query:req.params.query,
-		time:new Date()
-	})
-	res.json(req.params.query)
+    history.push({
+        query: req.params.query,
+        time: new Date()
+    })
+    var page = req.query.offset || 1
+    var url = 'https://api.imgur.com/3/gallery/search/top/'+page+'?q=' + req.params.query;
+    console.log(url)
+    console.log(config.clientId)
+    request({
+        method: 'GET',
+        url: url,
+        headers: {
+            Authorization: 'Client-ID ' + config.clientId
+        }
+    }, function(err, response, body) {
+    	var result = JSON.parse(body);
+    	res.json(result.data)
+    })
 });
 
 router.get('/api/latest/imagesearch', function(req, res, next) {
-	res.json(history)
+    res.json(history)
 });
 
 module.exports = router;
